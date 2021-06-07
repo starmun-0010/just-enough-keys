0787b8343b0c5fc4bae6e88405dc317a2796498b
muqsit <abdemuqsit@hotmail.com>
2021-06-07 23:02:40 +0500

    Cleanup

--- common/src/main/java/xyz/starmun/justenoughkeys/JustEnoughKeys.java
+++ common/src/main/java/xyz/starmun/justenoughkeys/JustEnoughKeys.java
@@ -1,37 +1,16 @@
 package xyz.starmun.justenoughkeys;
 
-import me.shedaniel.architectury.registry.CreativeTabs;
-import me.shedaniel.architectury.registry.DeferredRegister;
-import me.shedaniel.architectury.registry.Registries;
-import me.shedaniel.architectury.registry.RegistrySupplier;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.LazyLoadedValue;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 
-import java.util.function.Supplier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class JustEnoughKeys {
     public static final String MOD_ID = "justenoughkeys";
+    public static final String MOD_NAME = "Just Enough Keys";
     // We can use this if we don't want to use DeferredRegister
-    public static final LazyLoadedValue<Registries> REGISTRIES = new LazyLoadedValue<>(() -> Registries.get(MOD_ID));
-    // Registering a new creative tab
-    public static final CreativeModeTab EXAMPLE_TAB = CreativeTabs.create(new ResourceLocation(MOD_ID, "example_tab"), new Supplier<ItemStack>() {
-        @Override
-        public ItemStack get() {
-            return new ItemStack(EXAMPLE_ITEM.get());
-        }
-    });
-    
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registry.ITEM_REGISTRY);
-    public static final RegistrySupplier<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () ->
-            new Item(new Item.Properties().tab(JustEnoughKeys.EXAMPLE_TAB)));
-    
+    public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
     public static void init() {
-        ITEMS.register();
-        
+        LOGGER.debug("Loading Key Bindings");
         System.out.println(ExampleExpectPlatform.getConfigDirectory().toAbsolutePath().normalize().toString());
     }
 }
