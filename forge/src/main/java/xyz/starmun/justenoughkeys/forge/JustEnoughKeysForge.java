57257c13eafe59da95f3ab7fdc8539e7ef3098b5
muqsit <abdemuqsit@hotmail.com>
2021-06-19 23:59:39 +0500

    Fixed required on server bug

--- forge/src/main/java/xyz/starmun/justenoughkeys/forge/JustEnoughKeysForge.java
+++ forge/src/main/java/xyz/starmun/justenoughkeys/forge/JustEnoughKeysForge.java
@@ -1,14 +1,18 @@
 package xyz.starmun.justenoughkeys.forge;
 
 import me.shedaniel.architectury.platform.forge.EventBuses;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 import xyz.starmun.justenoughkeys.common.JustEnoughKeys;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-
+import org.apache.commons.lang3.tuple.Pair;
 @Mod(JustEnoughKeys.MOD_ID)
 public class JustEnoughKeysForge {
     public JustEnoughKeysForge() {
         // Submit our event bus to let architectury register our content on the right time
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         EventBuses.registerModEventBus(JustEnoughKeys.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         JustEnoughKeys.init();
     }
