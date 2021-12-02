88475be8d1c643a3c46657b52eae01ee55f0d583
muqsit <abdemuqsit@hotmail.com>
2021-12-02 18:03:46 +0500

    Removed mod specific compat, wasn't needed

--- forge/src/main/java/xyz/starmun/justenoughkeys/forge/JustEnoughKeysForge.java
+++ forge/src/main/java/xyz/starmun/justenoughkeys/forge/JustEnoughKeysForge.java
@@ -4,12 +4,10 @@ import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import xyz.starmun.justenoughkeys.common.JustEnoughKeys;
 import net.minecraftforge.fml.common.Mod;
-import xyz.starmun.justenoughkeys.forge.compat.SophisticatedBackpacksCompat;
 
 @Mod(JustEnoughKeys.MOD_ID)
 public class JustEnoughKeysForge {
     public JustEnoughKeysForge() {
-        SophisticatedBackpacksCompat.init();
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "", (a, b) -> true));
         JustEnoughKeys.init();
     }
