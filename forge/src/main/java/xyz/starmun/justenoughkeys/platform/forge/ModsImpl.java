82b49814a1728a1862ff64665b841a677289d294
starmun <abdemuqsit@homail.com>
2021-12-19 14:20:39 +0500

    Added Forge plugin config

--- forge/src/main/java/xyz/starmun/justenoughkeys/platform/forge/ModsImpl.java
+++ forge/src/main/java/xyz/starmun/justenoughkeys/platform/forge/ModsImpl.java
@@ -4,6 +4,9 @@ import net.minecraftforge.fml.ModList;
 
 public class ModsImpl {
     public static boolean isLoaded(String modID){
+        if(ModList.get() == null){
+            return false;
+        }
         return ModList.get().isLoaded(modID);
     }
 }
